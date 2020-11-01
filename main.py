ec173f8a87d26be7e000fdd77715320caa343b7c
Bhimashankar Prakash Javalkote <72732496+MrAkashCoder@users.noreply.github.com>
2020-11-01 23:05:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from typing import Optional
+
+from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/users/me")
-def read_current_user(username: str = Depends(get_current_username)):
-    return {"username": username}
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
