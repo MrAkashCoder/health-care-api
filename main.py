736afaf45ce3da1e498b0c202f30a1050165fab9
Bhimashankar Prakash Javalkote <72732496+MrAkashCoder@users.noreply.github.com>
2020-12-15 18:27:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/news/{news_url}")
+def read_item(news_url: str):
+    return {"news_url": news_url}
